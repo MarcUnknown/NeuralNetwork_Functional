2256b1bfa70da80178f94f4cd7ebd118527977ed
MarcUnknown <marc.oedingen.ta12@gmx.de>
2022-04-16 13:14:32 +0200

    Matrix Multiplication functional

--- src/MatrixMath.kt
+++ src/MatrixMath.kt
@@ -5,34 +5,14 @@ import kotlin.streams.toList
 class MatrixMath {
     fun dot(matrix_1: Matrix, matrix_2: Matrix): Matrix {
         require(matrix_1.getColumns() == matrix_2.getRows()) { "Rows does not match columns!" }
-
-        val elements = MutableList(matrix_1.getRows()) { MutableList(matrix_2.getColumns()) {0.0} }
-        for (i in 0 until matrix_1.getRows()) {
-            for (j in 0 until matrix_2.getColumns()) {
-                for (k in 0 until matrix_1.getColumns()) {
-                    elements[i][j] += matrix_1.getElements()[i][k] * matrix_2.getElements()[k][j]
-                }
-            }
-        }
-
-        // return Matrix(List(matrix_2.getRows()) { matrix_2_rowIndex ->
-        //     List(matrix_1.getColumns()) { matrix_1_columnIndex ->
-        //         List(matrix_2.getColumns()) { matrix_2_columnIndex ->
-        //             matrix_2.getElements()[matrix_2_rowIndex][matrix_2_columnIndex] + matrix_1.getElements()[matrix_2_columnIndex][matrix_1_columnIndex]
-        //         }.toMutableList()
-        //     }.toMutableList()
-        // }.toMutableList())
-
-        /*
-        return Matrix(List(matrix_1.getRows()) { rowMatrix1_Index ->
-            List(matrix_2.getColumns()) { columnMatrix2_Index ->
-                matrix_1.getElements().mapIndexed( {columnMatrix1_Index, elements -> matrix_1.getElements()[rowMatrix1_Index][columnMatrix1_Index] * matrix_2.getElements()[columnMatrix1_Index][columnMatrix2_Index] })
+        return Matrix(List(matrix_1.getRows()) { matrix_1_rowIndex ->
+            List(matrix_2.getColumns()) { matrix_2_columnIndex ->
+                List(matrix_1.getColumns()) { matrix_1_columnIndex ->
+                    matrix_1.getElements()[matrix_1_rowIndex][matrix_1_columnIndex] * matrix_2.getElements()[matrix_1_columnIndex][matrix_2_columnIndex]
+                }.toMutableList().fold(0.0) {acc, d ->
+                    acc + d}
             }.toMutableList()
-        }.toMutableList()
-        )
-        // matrix_1.getElements()[].map { columnMatrix1_Index -> matrix_1.getElements()[rowMatrix1_Index][columnMatrix1_Index] * matrix_2.getElements()[columnMatrix1_Index][columnMatrix2_Index] }
-        */
-        return Matrix(elements)
+        }.toMutableList())
     }
 
     fun hadamardDot(matrix_1: Matrix, matrix_2: Matrix): Matrix {
